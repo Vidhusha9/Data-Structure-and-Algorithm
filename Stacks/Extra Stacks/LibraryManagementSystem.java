0100e142456399a159064bbecce9d32cefc553ea
Vidhusha Vathsalya <vidhushavathsalya9@gmail.com>
2025-04-02 18:56:01 +0530

    Add files via upload

---
+++ Stacks/Extra Stacks/LibraryManagementSystem.java
@@ -0,0 +1,13 @@
+package Stacks;
+import java.util.Stack;
+
+public class LibraryManagementSystem {
+
+    public static void main(String[] args) {
+        Stack<String> dynamicArray = new Stack<>();
+
+        dynamicArray.push("Data Structure & Algorithm");
+        dynamicArray.push("Artificial Intelligence");
+        dynamicArray.push("Operating System");
+    }
+}
