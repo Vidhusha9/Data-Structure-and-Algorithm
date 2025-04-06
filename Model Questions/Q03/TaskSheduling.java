6fb10c891023065d43994e2deefb9a6263e21e0b
Vidhusha Vathsalya <vidhushavathsalya9@gmail.com>
2025-04-07 00:58:30 +0530

    Add files via upload

---
+++ Model Questions/Q03/TaskSheduling.java
@@ -0,0 +1,13 @@
+package Q03;
+
+public class TaskSheduling {
+    public int taskId;
+    public String taskName;
+    public int priorityLevel;
+
+    public TaskSheduling(int taskId, String taskName, int priorityLevel){
+        this.taskId = taskId;
+        this.taskName = taskName;
+        this.priorityLevel = priorityLevel;
+    }
+}
